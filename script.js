7b5ffb206d9fb3e540a3b9fd0ac07ec8803969b4
Addie Jackson <addieljackson3@gmail.com>
2020-08-07 19:21:40 -0400

    Project complete :)

---
+++ script.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const AnyList = require('anylist');
+const fs = require('fs')
+const {
+    google
+} = require('googleapis');
+const sheets = google.sheets('v4');
+
+async function main() {
+    const authClient = await authorize();
+    const request = {
+        // The ID of the spreadsheet to retrieve data from.
+        spreadsheetId: process.env.SPREADSHEET_ID,
+
+        // The A1 notation of the values to retrieve.
+        range: process.env.SPREADSHEET_RANGE,
+        auth: authClient,
+    };
+
+    try {
+        const response = (await sheets.spreadsheets.values.get(request)).data;
+        let grocery_items = response.values.map(function (x) {
+            return {
+                name: x[0],
+                category: x[1]
+            };
+        })
+
+        const any = new AnyList({
+            email: process.env.ANYLIST_EMAIL,
+            password: process.env.ANYLIST_PASSWORD
+        });
+        any.login().then(async () => {
+            await any.getLists();
+
+            // Add new item to the grocery list
+            const grocery_list = any.getListByName('Grocery List');
+
+            for (item of grocery_items) {
+                let g_item = any.createItem({
+                    name: item.name,
+                    categoryMatchId: item.category.toLowerCase()
+                });
+
+                await grocery_list.addItem(g_item)
+            }
+            any.teardown();
+        })
+
+    } catch (err) {
+        console.error(err);
+    }
+};
+main();
+
+async function authorize() {
+    const credentials = JSON.parse(fs.readFileSync(process.env.ROOT_DIRECTORY + 'credentials.json', 'utf-8'));
+
+    const {
+        client_secret: clientSecret,
+        client_id: clientId,
+        redirect_uris: redirectUris,
+    } = credentials.installed;
+
+    const oAuth2Client = new google.auth.OAuth2(
+        clientId, clientSecret, redirectUris[0],
+    );
+
+    const token = fs.readFileSync(process.env.ROOT_DIRECTORY + 'token.json', 'utf-8');
+    oAuth2Client.setCredentials(JSON.parse(token));
+
+    return oAuth2Client;
+}
